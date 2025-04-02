17f812ba2785f1bc3219b2ac48b98359d784b328
vijay.mr <vijay.mr@grootan.com>
2025-04-02 18:07:26 +0530

    Update package swift file

--- Package.swift
+++ Package.swift
@@ -6,16 +6,21 @@ import PackageDescription
 let package = Package(
     name: "EztoCore",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EztoCore",
             targets: ["EztoCore"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "EztoVerifyCore",
+            path: "./EztoVerifyCore.xcframework"
+        ),
         .target(
-            name: "EztoCore"),
-
+            name: "EztoCore",
+            dependencies: [
+                .target(name: "EztoVerifyCore"),
+            ],
+            path: "Sources"
+        ),
     ]
 )
